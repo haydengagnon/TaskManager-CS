24acbb59cb54eeb260fc23c5abda0d592407cbc5
Hayden Gagnon <haydengagnon1@gmail.com>
2023-08-03 14:07:53 -0500

    more setup stuff

---
+++ Forms/Form1.cs
@@ -0,0 +1,18 @@
+using System;
+using System.Drawing;
+using System.Windows.Forms;
+
+namespace TaskManager_CS;
+
+public partial class Form1 : Form
+{
+    private const int SCREEN_WIDTH = 1280;
+    private const int SCREEN_HEIGHT = 720;
+
+    public Form1()
+    {
+        InitializeComponent();
+        this.StartPosition = FormStartPosition.CenterScreen;
+        this.ClientSize = new Size(SCREEN_WIDTH, SCREEN_HEIGHT);
+    }
+}
