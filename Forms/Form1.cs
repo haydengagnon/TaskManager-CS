916db1bbf12c3af1f7fc5aee4becaabb97fdec8c
Hayden Gagnon <haydengagnon1@gmail.com>
2023-08-10 15:04:44 -0500

    small additions to Form1

--- Forms/Form1.cs
+++ Forms/Form1.cs
@@ -6,8 +6,8 @@ namespace TaskManager_CS;
 
 public partial class Form1 : Form
 {
-    private const int SCREEN_WIDTH = 1280;
-    private const int SCREEN_HEIGHT = 720;
+    private const int SCREEN_WIDTH = 600;
+    private const int SCREEN_HEIGHT = 800;
 
     public Form1()
     {
@@ -17,8 +17,26 @@ public partial class Form1 : Form
         this.DoubleBuffered = true;
         this.BackColor = Color.Black;
 
-        Label toDo = new Label();
-        toDo.Text = "Tasks";
-        
+        Label home = new Label();
+        home.Text = "Home";
+        home.ForeColor = Color.White;
+        home.BackColor = Color.Black;
+        home.Font = new Font(home.Font.FontFamily, 32);
+        home.Size = new Size(SCREEN_WIDTH, 100);
+        home.TextAlign = ContentAlignment.MiddleCenter;
+
+        Button addTask = new Button();
+        addTask.Text = "Add New Task";
+        addTask.ForeColor = Color.Black;
+        addTask.BackColor = Color.Green;
+        addTask.Font = new Font(addTask.Font.FontFamily, 20);
+        addTask.Size = new Size(150, 100);
+        addTask.Location = new Point(SCREEN_WIDTH - 160, SCREEN_HEIGHT - 110);
+        addTask.TextAlign = ContentAlignment.MiddleCenter;
+
+
+
+        this.Controls.Add(home);
+        this.Controls.Add(addTask);
     }
 }
