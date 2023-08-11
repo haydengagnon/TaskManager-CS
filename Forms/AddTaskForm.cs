d9c73907ffb258952aa619f3164ccf7d9265ab80
Hayden Gagnon <haydengagnon1@gmail.com>
2023-08-11 12:43:09 -0500

    Working on AddTaskForm.cs     Added textboxes, date picker and button with no functionality

--- Forms/AddTaskForm.cs
+++ Forms/AddTaskForm.cs
@@ -8,28 +8,50 @@ namespace TaskManager_CS
     {
         private const int SCREEN_WIDTH = 600;
         private const int SCREEN_HEIGHT = 800;
-        private TextBox titleTextBox;
-        private TextBox descriptionTextBox;
-        private DateTimePicker dueDatePicker;
-        private Button addButton;
+        TextBox titleTextBox = new System.Windows.Forms.TextBox();
+        TextBox descriptionTextBox = new System.Windows.Forms.TextBox();
+        DateTimePicker dueDatePicker = new DateTimePicker();
+        Button addButton = new Button();
 
         public AddTaskForm()
         {
             InitializeComponent();
-            initiate();
-            
-            void initiate()
-            {
+                     
             this.StartPosition = FormStartPosition.CenterScreen;
             this.ClientSize = new Size(SCREEN_WIDTH, SCREEN_HEIGHT);
             this.DoubleBuffered = true;
             this.BackColor = Color.Black;
 
+            titleTextBox.Size = new Size(SCREEN_WIDTH - 10, 200);
+            titleTextBox.Location = new Point(5, 5);
             titleTextBox.PlaceholderText = "Title";
             titleTextBox.AcceptsReturn = true;
             titleTextBox.AcceptsTab = true;
+            titleTextBox.Font = new Font(titleTextBox.Font.FontFamily, 28);
             this.Controls.Add(titleTextBox);
-            }
+            
+            descriptionTextBox.Size = new Size(SCREEN_WIDTH - 10, SCREEN_HEIGHT - 200);
+            descriptionTextBox.AutoSize = false;
+            descriptionTextBox.AcceptsReturn = true;
+            descriptionTextBox.Multiline = true;
+            descriptionTextBox.Location = new Point(5, 65);
+            descriptionTextBox.PlaceholderText = "Description";
+            this.Controls.Add(descriptionTextBox);
+
+            dueDatePicker.Size = new Size(SCREEN_WIDTH - 10, 100);
+            dueDatePicker.Font = new Font(dueDatePicker.Font.FontFamily, 28);
+            dueDatePicker.Location = new Point(5, 670);
+            this.Controls.Add(dueDatePicker);
+
+            addButton.Size = new Size(SCREEN_WIDTH - 10, 50);
+            addButton.Font = new Font(addButton.Font.FontFamily, 28);
+            addButton.Location = new Point(5, SCREEN_HEIGHT - 60);
+            addButton.Text = "Submit Task";
+            addButton.TextAlign = ContentAlignment.MiddleCenter;
+            addButton.ForeColor = Color.White;
+            addButton.BackColor = Color.Green;
+            addButton.Click += new EventHandler(onButtonClickedAddButon);
+            this.Controls.Add(addButton);
         }
     }
 }
